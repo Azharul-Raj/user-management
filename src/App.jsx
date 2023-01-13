f71d356cb618abedad5014d077aa06ed4179dcc2
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-13 15:37:04 +0600

    setup firebase condig

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import HomePage from './pages/Main/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-red-500'>Hey there</h1>
+      <HomePage/>
     </div>
   )
 }
