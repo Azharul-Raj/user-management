84feed377a6ec68fd3cdac6d5c35a8d8590d6a92
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-15 11:37:34 +0600

    adding nested dropdown

--- src/contexts/DataProvider.jsx
+++ src/contexts/DataProvider.jsx
@@ -2,12 +2,17 @@ import { createContext, useState } from "react";
 
 export const DataContext=createContext()
 const DataProvider = ({ children }) => {
-    const [id1, setId] = useState("EVT0001")
     const [refresh, setRefresh] = useState(true);
-    let [maleCount,setMaleCount]=useState(0)
-    const [femaleCount,setFemaleCount]=useState(0)
-    
-    const data={id1,setId,refresh,setRefresh,maleCount,setMaleCount,femaleCount,setFemaleCount}
+    let [maleCount, setMaleCount] = useState(0);
+    const [femaleCount, setFemaleCount] = useState(0);
+    const [filter, setFilter] = useState("");
+    if(filter===""){
+    var [id1, setId] = useState("EVT0001")
+    }
+    else if (filter === "Male") {
+        var [id1, setId] = useState("EVT0002")
+    }
+    const data={id1,setId,refresh,setRefresh,maleCount,setMaleCount,femaleCount,setFemaleCount,filter,setFilter}
     return (
         <DataContext.Provider value={data}>
             {
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -9,11 +9,11 @@ import { DataContext } from "../../contexts/dataProvider";
 import { Outlet } from "react-router-dom";
 import { db } from "../../firebase/firebase.config";
 const Header = () => {
-    let { maleCount,femaleCount,setFemaleCount, setMaleCount } = useContext(DataContext);
+    let { filter,maleCount,femaleCount,setFemaleCount, setMaleCount } = useContext(DataContext);
     const [clients, setClients] = useState([]);
     const clientsMemo=useMemo(()=>clients,[clients])
     // data fetching part
-  const filter='Male'
+    console.log('check filter',filter);
     useEffect(() => {
       const fetchData = async () => {
         let list = [];
@@ -86,110 +86,24 @@ const Header = () => {
                 </div>
                 <div className="antialiased">
                   {/* dropdown */}
-                  <div className="group inline-block">
-                    <button
-                      aria-haspopup="true"
-                      aria-controls="menu"
-                      className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
-                    >
-                      <span className="pr-1 font-semibold flex-1">
-                        <FaSlidersH />
-                      </span>
-                      <span>
-                        <svg
-                          className="fill-current h-4 w-4 transform group-hover:-rotate-180
-        transition duration-150 ease-in-out"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                        </svg>
-                      </span>
-                    </button>
-                    <ul
-                      id="menu"
-                      aria-hidden="true"
-                      className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
-  transition duration-150 ease-in-out origin-top min-w-32"
-                    >
-                      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-                        Programming
-                      </li>
-                      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-                        DevOps
-                      </li>
-                      <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-                        <button
-                          aria-haspopup="true"
-                          aria-controls="menu-lang"
-                          className="w-full text-left flex items-center outline-none focus:outline-none"
-                        >
-                          <span className="pr-1 flex-1">Langauges</span>
-                          <span className="mr-auto">
-                            <svg
-                              className="fill-current h-4 w-4
-            transition duration-150 ease-in-out"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                            >
-                              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                          </span>
-                        </button>
-                        <ul
-                          id="menu-lang"
-                          aria-hidden="true"
-                          className="bg-white border rounded-sm absolute top-0 right-0 
-  transition duration-150 ease-in-out origin-top-left
-  min-w-32
-  "
-                        >
-                          <li className="px-3 py-1 hover:bg-gray-100">
-                            Javascript
-                          </li>
-                          <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-                            <button
-                              aria-haspopup="true"
-                              aria-controls="menu-lang-python"
-                              className="w-full text-left flex items-center outline-none focus:outline-none"
-                            >
-                              <span className="pr-1 flex-1">Python</span>
-                              <span className="mr-auto">
-                                <svg
-                                  className="fill-current h-4 w-4
-                transition duration-150 ease-in-out"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 20 20"
-                                >
-                                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                </svg>
-                              </span>
-                            </button>
-                            <ul
-                              id="menu-lang-python"
-                              aria-hidden="true"
-                              className="bg-white border rounded-sm absolute top-0 right-0 
-      transition duration-150 ease-in-out origin-top-left
-      min-w-32
-      "
-                            >
-                              <li className="px-3 py-1 hover:bg-gray-100">
-                                2.7
-                              </li>
-                              <li className="px-3 py-1 hover:bg-gray-100">
-                                3+
-                              </li>
-                            </ul>
-                          </li>
-                          <li className="px-3 py-1 hover:bg-gray-100">Go</li>
-                          <li className="px-3 py-1 hover:bg-gray-100">Rust</li>
-                        </ul>
-                      </li>
-                      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-                        Testing
-                      </li>
-                    </ul>
-                  </div>
+                  <div class="space-y-0.5">
+  <select
+    id="basic"
+    name="basic"
+    class="">
+    <option selected><FaSlidersH/></option>
+                      <option>
+                      <div className="dropdown dropdown-end">
+  <label tabIndex={0} className="btn m-1">Click</label>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
+    </option>
+    <option>Tomato</option>
+  </select>
+</div>
                   {/* dropdown */}
                 </div>
               </div>
